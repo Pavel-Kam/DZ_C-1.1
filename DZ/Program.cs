0f18c57c0caccffe7940a18ec8393bb077a4577f
Pavel-Kam <pavel.nokt@gmail.com>
2022-07-14 20:31:14 +0300

    Выполнен

---
+++ DZ/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первую цифру: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите вторую цифру: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x>y)
+{
+Console.Write(x);
+}
+else
+{
+Console.Write("Максимальное значение:");
+Console.Write(y);
+}
